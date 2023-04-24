fa84cccf5df170551d5c19ca594a02a841812ec8
BeanGreen247 <mozdrent@gmail.com>
2023-04-24 17:07:28 +0200

    + improved syntax highlighting + added Java as a supported language + added updated issues section into readme

--- tinGUI/tinGUI.py
+++ tinGUI/tinGUI.py
@@ -6,33 +6,107 @@ import idlelib.percolator as ip
 import idlelib.colorizer as ic
 import re
 
+version='v0.1.alpha'
+# color def
+bgColor = '#292929'
+pythonCommentColor = '#FF0000'
+CommentColor = '#57A546'
+CLOUD2 = '#7f9da5'
+LEMON = '#FAFA33'
+OVERCAST = '#c3bdab'
+PUMPKIN = '#ff7518'
+STORMY = '#5a6caa'
+SAILOR = '#38739D'
+CLOUD = '#c7c4bf'
+DK_SEAFOAM = '#9FE2BF'
+PURPLE = '#A020F0'
+
 class TextSyntax:
-    cdg = ic.ColorDelegator()
-    cdg.prog = re.compile(r'\b(?P<MYGROUP>tkinter)\b|' + ic.make_pat(), re.S)
-    cdg.idprog = re.compile(r'\s+(\w+)', re.S)
+    cd = ic.ColorDelegator()
 
-    cdg.tagdefs['MYGROUP'] = {'foreground': '#ffffff', 'background': '#292929'}
-
-    # These five lines are optional. If omitted, default colours are used.
-    cdg.tagdefs['COMMENT'] = {'foreground': '#ffffff', 'background': '#292929'}
-    cdg.tagdefs['KEYWORD'] = {'foreground': '#ffffff', 'background': '#292929'}
-    cdg.tagdefs['BUILTIN'] = {'foreground': '#ffffff', 'background': '#292929'}
-    cdg.tagdefs['STRING'] = {'foreground': '#ffffff', 'background': '#292929'}
-    cdg.tagdefs['DEFINITION'] = {'foreground': '#ffffff', 'background': '#292929'}
+    cd.tagdefs['COMMENT'] = {'foreground': '#ffffff', 'background': bgColor}
+    cd.tagdefs['KEYWORD'] = {'foreground': '#ffffff', 'background': bgColor}
+    cd.tagdefs['BUILTIN'] = {'foreground': '#ffffff', 'background': bgColor}
+    cd.tagdefs['STRING'] = {'foreground': '#ffffff', 'background': bgColor}
+    cd.tagdefs['DEFINITION'] = {'foreground': '#ffffff', 'background': bgColor}
 
 class PythonSyntax:
-    cdg = ic.ColorDelegator()
-    cdg.prog = re.compile(r'\b(?P<MYGROUP>tkinter)\b|' + ic.make_pat(), re.S)
-    cdg.idprog = re.compile(r'\s+(\w+)', re.S)
+    #syntax highlighter patterns
+    KEYWORD   = r"\b(?P<KEYWORD>False|None|True|and|as|assert|async|await|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|raise|return|try|while|with|yield)\b"
+    EXCEPTION = r"([^.'\"\\#]\b|^)(?P<EXCEPTION>ArithmeticError|AssertionError|AttributeError|BaseException|BlockingIOError|BrokenPipeError|BufferError|BytesWarning|ChildProcessError|ConnectionAbortedError|ConnectionError|ConnectionRefusedError|ConnectionResetError|DeprecationWarning|EOFError|Ellipsis|EnvironmentError|Exception|FileExistsError|FileNotFoundError|FloatingPointError|FutureWarning|GeneratorExit|IOError|ImportError|ImportWarning|IndentationError|IndexError|InterruptedError|IsADirectoryError|KeyError|KeyboardInterrupt|LookupError|MemoryError|ModuleNotFoundError|NameError|NotADirectoryError|NotImplemented|NotImplementedError|OSError|OverflowError|PendingDeprecationWarning|PermissionError|ProcessLookupError|RecursionError|ReferenceError|ResourceWarning|RuntimeError|RuntimeWarning|StopAsyncIteration|StopIteration|SyntaxError|SyntaxWarning|SystemError|SystemExit|TabError|TimeoutError|TypeError|UnboundLocalError|UnicodeDecodeError|UnicodeEncodeError|UnicodeError|UnicodeTranslateError|UnicodeWarning|UserWarning|ValueError|Warning|WindowsError|ZeroDivisionError)\b"
+    BUILTIN   = r"([^.'\"\\#]\b|^)(?P<BUILTIN>abs|all|any|ascii|bin|breakpoint|callable|chr|classmethod|compile|complex|copyright|credits|delattr|dir|divmod|enumerate|eval|exec|exit|filter|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|isinstance|issubclass|iter|len|license|locals|map|max|memoryview|min|next|oct|open|ord|pow|print|quit|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|sum|type|vars|zip)\b"
+    DOCSTRING = r"(?P<DOCSTRING>(?i:r|u|f|fr|rf|b|br|rb)?'''[^'\\]*((\\.|'(?!''))[^'\\]*)*(''')?|(?i:r|u|f|fr|rf|b|br|rb)?\"\"\"[^\"\\]*((\\.|\"(?!\"\"))[^\"\\]*)*(\"\"\")?)"
+    STRING    = r"(?P<STRING>(?i:r|u|f|fr|rf|b|br|rb)?'[^'\\\n]*(\\.[^'\\\n]*)*'?|(?i:r|u|f|fr|rf|b|br|rb)?\"[^\"\\\n]*(\\.[^\"\\\n]*)*\"?)"
+    TYPES     = r"\b(?P<TYPES>bool|bytearray|bytes|dict|float|int|list|str|tuple|object)\b"
+    NUMBER    = r"\b(?P<NUMBER>((0x|0b|0o|#)[\da-fA-F]+)|((\d*\.)?\d+))\b"
+    CLASSDEF  = r"(?<=\bclass)[ \t]+(?P<CLASSDEF>\w+)[ \t]*[:\(]" #recolor of DEFINITION for class definitions
+    DECORATOR = r"(^[ \t]*(?P<DECORATOR>@[\w\d\.]+))"
+    INSTANCE  = r"\b(?P<INSTANCE>super|self|cls)\b"
+    COMMENT   = r"(?P<COMMENT>#[^\n]*)"
+    SYNC      = r"(?P<SYNC>\n)"
 
-    cdg.tagdefs['MYGROUP'] = {'foreground': '#bf3434', 'background': '#292929'}
+    PROG   = rf"{KEYWORD}|{BUILTIN}|{EXCEPTION}|{TYPES}|{COMMENT}|{DOCSTRING}|{STRING}|{SYNC}|{INSTANCE}|{DECORATOR}|{NUMBER}|{CLASSDEF}"
 
-    # These five lines are optional. If omitted, default colours are used.
-    cdg.tagdefs['COMMENT'] = {'foreground': '#FF0000', 'background': '#292929'}
-    cdg.tagdefs['KEYWORD'] = {'foreground': '#007F00', 'background': '#292929'}
-    cdg.tagdefs['BUILTIN'] = {'foreground': '#7F7F00', 'background': '#292929'}
-    cdg.tagdefs['STRING'] = {'foreground': '#7F3F00', 'background': '#292929'}
-    cdg.tagdefs['DEFINITION'] = {'foreground': '#007F7F', 'background': '#292929'}
+    IDPROG = r"(?<!class)\s+(\w+)"
+
+    TAGDEFS   = {   
+                'COMMENT'    : {'foreground': pythonCommentColor  , 'background': bgColor},
+                'TYPES'      : {'foreground': CLOUD2    , 'background': bgColor},
+                'NUMBER'     : {'foreground': LEMON     , 'background': bgColor},
+                'BUILTIN'    : {'foreground': OVERCAST  , 'background': bgColor},
+                'STRING'     : {'foreground': PUMPKIN   , 'background': bgColor},
+                'DOCSTRING'  : {'foreground': STORMY    , 'background': bgColor},
+                'EXCEPTION'  : {'foreground': CLOUD2    , 'background': bgColor},
+                'DEFINITION' : {'foreground': SAILOR    , 'background': bgColor},
+                'DECORATOR'  : {'foreground': CLOUD2    , 'background': bgColor},
+                'INSTANCE'   : {'foreground': CLOUD     , 'background': bgColor},
+                'KEYWORD'    : {'foreground': DK_SEAFOAM, 'background': bgColor},
+                'CLASSDEF'   : {'foreground': PURPLE    , 'background': bgColor},
+            }
+
+    cd         = ic.ColorDelegator()
+    cd.prog    = re.compile(PROG, re.S|re.M)
+    cd.idprog  = re.compile(IDPROG, re.S)
+    cd.tagdefs = {**cd.tagdefs, **TAGDEFS}
+
+class JavaSyntax:
+    #syntax highlighter patterns
+    KEYWORD   = r"\b(?P<KEYWORD>False|None|True|abstract|assert|boolean|break|byte|case|catch|char|class|continue|default|do|double|else|enum|extends|final|finally|float|for|if|implements|import|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\b"
+    EXCEPTION = r"([^.'\"\\#]\b|^)(?P<EXCEPTION>CloneNotSupportedException|InterruptedException|ReflectiveOperationException|ClassNotFoundException|IllegalAccessException|InstantiationException|NoSuchFieldException|NoSuchMethodException|RuntimeException|ArithmeticException|ArrayStoreException|ClassCastException|EnumConstantNotPresentException|IllegalArgumentException|IllegalThreadStateException|NumberFormatException|IllegalMonitorStateException|IllegalStateException|IndexOutOfBoundsException|ArrayIndexOutOfBoundsException|StringIndexOutOfBoundsException|NegativeArraySizeException|NullPointerException|SecurityException|TypeNotPresentException|UnsupportedOperationException)\b"
+    BUILTIN   = r"([^.'\"\\#]\b|^)(?P<BUILTIN>charAt|codePointAt|codePointBefore|codePointCount|compareTo|compareToIgnoreCase|concat|contains|contentEquals|copyValueOf|endsWith|equals|equalsIgnoreCase|format|getBytes|getChars|hashCode|indexOf|intern|isEmpty|lastIndexOf|length|matches|offsetByCodePoints|regionMatches|replace|replaceFirst|replaceAll|split|startsWith|subSequence|substring|toCharArray|toLowerCase|toString|toUpperCase|trim|valueOf|abs|acos|asin|atan|atan2|cbrt|ceil|copySign|cos|cosh|exp|expm1|floor|getExponent|hypot|IEEEremainder|log|log10|log1p|max|min|nextAfter|nextUp|pow|random|round|rint|signum|sin|sinh|sqrt|tan|tanh|toDegrees|toRadians|ulp)\b"
+    DOCSTRING = r"(?P<DOCSTRING>(?i:r|u|f|fr|rf|b|br|rb)?'''[^'\\]*((\\.|'(?!''))[^'\\]*)*(''')?|(?i:r|u|f|fr|rf|b|br|rb)?\"\"\"[^\"\\]*((\\.|\"(?!\"\"))[^\"\\]*)*(\"\"\")?)"
+    STRING    = r"(?P<STRING>(?i:r|u|f|fr|rf|b|br|rb)?'[^'\\\n]*(\\.[^'\\\n]*)*'?|(?i:r|u|f|fr|rf|b|br|rb)?\"[^\"\\\n]*(\\.[^\"\\\n]*)*\"?)"
+    TYPES     = r"\b(?P<TYPES>byte|short|int|long|float|double|boolean|char)\b"
+    NUMBER    = r"\b(?P<NUMBER>((0x|0b|0o|#)[\da-fA-F]+)|((\d*\.)?\d+))\b"
+    CLASSDEF  = r"(?<=\bclass)[ \t]+(?P<CLASSDEF>\w+)[ \t]*[:\(]" #recolor of DEFINITION for class definitions
+    DECORATOR = r"(^[ \t]*(?P<DECORATOR>@[\w\d\.]+))"
+    INSTANCE  = r"\b(?P<INSTANCE>self)\b"
+    COMMENT   = r"(?P<COMMENT>//[^\n]*)"
+    SYNC      = r"(?P<SYNC>\n)"
+
+    PROG   = rf"{KEYWORD}|{BUILTIN}|{EXCEPTION}|{TYPES}|{COMMENT}|{DOCSTRING}|{STRING}|{SYNC}|{INSTANCE}|{DECORATOR}|{NUMBER}|{CLASSDEF}"
+
+    IDPROG = r"(?<!class)\s+(\w+)"
+
+    TAGDEFS   = {   
+                'COMMENT'    : {'foreground': CommentColor  , 'background': bgColor},
+                'TYPES'      : {'foreground': CLOUD2    , 'background': bgColor},
+                'NUMBER'     : {'foreground': LEMON     , 'background': bgColor},
+                'BUILTIN'    : {'foreground': OVERCAST  , 'background': bgColor},
+                'STRING'     : {'foreground': PUMPKIN   , 'background': bgColor},
+                'DOCSTRING'  : {'foreground': STORMY    , 'background': bgColor},
+                'EXCEPTION'  : {'foreground': CLOUD2    , 'background': bgColor},
+                'DEFINITION' : {'foreground': SAILOR    , 'background': bgColor},
+                'DECORATOR'  : {'foreground': CLOUD2    , 'background': bgColor},
+                'INSTANCE'   : {'foreground': CLOUD     , 'background': bgColor},
+                'KEYWORD'    : {'foreground': DK_SEAFOAM, 'background': bgColor},
+                'CLASSDEF'   : {'foreground': PURPLE    , 'background': bgColor},
+            }
+
+    cd         = ic.ColorDelegator()
+    cd.prog    = re.compile(PROG, re.S|re.M)
+    cd.idprog  = re.compile(IDPROG, re.S)
+    cd.tagdefs = {**cd.tagdefs, **TAGDEFS}
 
 root = tk.Tk()
 # Creating Menubar
@@ -54,7 +128,7 @@ class Window:
              try:
               tf = open(tf)
               self.text.delete('1.0', END)
-              root.title("tinGUI - " + str(pathlib.Path(tf.name)))
+              root.title("tinGUI " + version + " - " + str(pathlib.Path(tf.name)))
               data = tf.read()
               self.text.insert(END, data)
               tf.close()
@@ -69,7 +143,7 @@ class Window:
              initialdir="C:/User/", #on linux opens /home/username/
              title="New file",
              filetypes=(("Any Files", "*"),))
-             root.title("tinGUI - " + str(pathlib.Path(tf.name)))
+             root.title("tinGUI " + version + " - " + str(pathlib.Path(tf.name)))
              self.text.delete('1.0', END)
              try:
               tf = open(tf)
@@ -106,32 +180,39 @@ class Window:
                 pass
 
         def guiGroup0():
-            self.text = tk.Text(self.frame, undo = True, height = 40, width = 140, bg='#292929', fg='#ffffff', insertbackground='#ffffff')
+            self.text = tk.Text(self.frame, undo = True, height = 40, width = 140, bg=bgColor, fg='#ffffff', insertbackground='#ffffff')
             self.text.pack(expand = True, fill = tk.BOTH)
             clearSyntaxFilters
 
         def clearSyntaxFilters():
             ip.Percolator(self.text).filters.clear()
-            ip.Percolator(self.text).filters.pop()
-            ip.Percolator(self.text).filters.remove()
             ip.Percolator(self.text).close()
-            ip.Percolator(self.text).delete()
             ip.Delegator.resetcache()
 
         def textSyntax():
             try:
-                ip.Percolator(self.text).insertfilter(TextSyntax.cdg)
+                ip.Percolator(self.text).insertfilter(TextSyntax.cd)
             except:
                 clearSyntaxFilters
-                ip.Percolator(self.text).insertfilter(TextSyntax.cdg)
+                ip.Percolator(self.text).open()
+                ip.Percolator(self.text).insertfilter(TextSyntax.cd)
             
 
         def pythonSyntax():
             try:
-                ip.Percolator(self.text).insertfilter(PythonSyntax.cdg)
+                ip.Percolator(self.text).insertfilter(PythonSyntax.cd)
             except:
                 clearSyntaxFilters
-                ip.Percolator(self.text).insertfilter(PythonSyntax.cdg)
+                ip.Percolator(self.text).open()
+                ip.Percolator(self.text).insertfilter(PythonSyntax.cd)
+
+        def javaSyntax():
+            try:
+                ip.Percolator(self.text).insertfilter(JavaSyntax.cd)
+            except:
+                clearSyntaxFilters
+                ip.Percolator(self.text).open()
+                ip.Percolator(self.text).insertfilter(JavaSyntax.cd)
         
         def guiGroup1(): 
             # Adding File Menu and commands
@@ -154,11 +235,12 @@ class Window:
             menubar.add_cascade(label ='Syntax', menu = syntax)
             syntax.add_command(label ='Plain Text', command = textSyntax)
             syntax.add_command(label ='Python', command = pythonSyntax)
+            syntax.add_command(label ='Java', command = javaSyntax)
  
         threading.Thread(target=guiGroup0).start()
         threading.Thread(target=guiGroup1).start()
 
-root.title("tinGUI")
+root.title("tinGUI " + version)
 root.config(menu = menubar)
 window = Window(root)
 root.mainloop()
